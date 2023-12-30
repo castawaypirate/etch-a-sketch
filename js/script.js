790a3f007422ff550827de4cfef3366a06b68f96
castawaypirate <a.sykiotis@mellongroup.com>
2023-12-30 12:20:12 +0200

    Implement main functionality and grid customization

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var slider = document.getElementById("rangeSlider");
+var output = document.getElementById("dimensions");
+output.innerHTML = slider.value +"x"+ slider.value;
+createSketchbook(slider.value); 
+
+slider.oninput = function() {
+  output.innerHTML = this.value +"x"+ this.value;
+  createSketchbook(this.value); 
+}
+
+function createSketchbook(side){
+    const sketch = document.querySelector(".sketchbook");
+    sketch.innerHTML = "";
+
+    let paint = "gray";
+
+    for(let i=0;i<side*side;i++){
+        const div = document.createElement("div");
+        div.classList.add("square");
+        div.style.width = 500/side + "px";
+        div.style.height = 500/side + "px";
+        div.addEventListener("mousedown", function() {
+            div.style.backgroundColor = paint;
+        });
+        div.addEventListener("mouseenter", (event) => {
+            if(event.buttons===1){
+                div.style.backgroundColor = paint;
+            }
+        });
+        sketch.appendChild(div);
+    }
+}
